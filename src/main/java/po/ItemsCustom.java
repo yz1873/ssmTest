918694c54bdcb6687a29c27084351cfce56c5e61
yz1873 <yz1873@126.com>
2017-02-16 12:12:48 +0800

    Initial commit

---
+++ src/main/java/po/ItemsCustom.java
@@ -0,0 +1,9 @@
+package po;
+
+/**
+ * Created by Zhang Yu on 2017/2/16.
+ */
+public class ItemsCustom extends Items {
+
+
+}
---
+++ src/main/java/po/ItemsQueryVo.java
@@ -0,0 +1,17 @@
+package po;
+
+/**
+ * Created by Zhang Yu on 2017/2/16.
+ */
+public class ItemsQueryVo {
+    //商品信息
+    private ItemsCustom itemsCustom;
+
+    public ItemsCustom getItemsCustom() {
+        return itemsCustom;
+    }
+
+    public void setItemsCustom(ItemsCustom itemsCustom) 	{
+        this.itemsCustom = itemsCustom;
+    }
+}
